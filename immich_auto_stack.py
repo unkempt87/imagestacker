61e22d157264d26e982b02aa444e337ddc3abf67
unkempt87 <unkempt1987@gmail.com>
2025-01-30 16:50:40 -0500

    Update immich_auto_stack.py

--- immich_auto_stack.py
+++ immich_auto_stack.py
@@ -237,11 +237,10 @@ def main():
     
     for child in stack[1:]:
       logger.info(f'   Child name:  {child["originalFileName"]} ID: {child["id"]}')
-    all_ids = []
-    all_ids = parent_id + children_id
+    parent_id.extend(children_id)
     if len(children_id) > 0:
       payload = {
-        "assetIds": all_ids,
+        "assetIds": parent_id,
       }
 
       if not dry_run:
